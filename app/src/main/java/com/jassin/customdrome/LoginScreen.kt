8cc7ada7f17506f56276218ff369c3b05b2315ce
probablyjassin <aouanijassin@gmail.com>
2026-05-11 20:14:02 +0200

    working on material you login screen

---
+++ app/src/main/java/com/jassin/customdrome/LoginScreen.kt
@@ -0,0 +1,49 @@
+package com.jassin.customdrome
+
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.Text
+import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.Column
+// Important: Import the Material3 version of components for Dynamic Color
+import androidx.compose.material3.MaterialTheme
+import com.jassin.customdrome.ui.theme.CustomDromeTheme
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.safeDrawingPadding
+
+@Composable
+fun LoginScreen() {
+    CustomDromeTheme {
+        // responsible for the themed bg color
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            // prevents overlap with the status bar
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .safeDrawingPadding(),
+                contentAlignment = Alignment.Center
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Login to CustomDrome",
+                        color = MaterialTheme.colorScheme.primary,
+                        style = MaterialTheme.typography.headlineMedium
+                    )
+
+                    Button(onClick = { /* logic goes here */ }) {
+                        Text("Login")
+                    }
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/jassin/customdrome/MainActivity.kt
+++ app/src/main/java/com/jassin/customdrome/MainActivity.kt
@@ -19,12 +19,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CustomDromeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                //Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                 //   Greeting(
+                 //       name = "Android",
+                 //       modifier = Modifier.padding(innerPadding)
+                 //   )
+                //}
+                LoginScreen()
             }
         }
     }
