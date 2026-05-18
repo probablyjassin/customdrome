921928a9520123b7f2deda9b4feae54807ce548b
probablyjassin <123012687+probablyjassin@users.noreply.github.com>
2026-05-18 20:26:27 +0200

    add dependencies for media playback

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,4 +71,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
     // Coil for image loading in Compose
     implementation("io.coil-kt:coil-compose:2.4.0")
+
+    implementation("androidx.media3:media3-exoplayer:1.5.1")
+    implementation("androidx.media3:media3-session:1.5.1")
+    implementation(libs.androidx.media3.datasource.okhttp)
 }
